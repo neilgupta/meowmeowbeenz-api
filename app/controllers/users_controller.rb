a8a179f5cbef6e921216881ea200b3bc3d4e5464
Neil Gupta <neil@tabuleapp.com>
2014-03-13 00:43:58 -0500

    Add sample responses to documentation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,14 @@ class UsersController < ApplicationController
   api :GET, '/users/:username', "Get details for a user, or get your own details at /users/me"
   param_group :token
   error 404, "User not found"
+  example <<-EOS
+    # The response will look like:
+    {
+      "username": "abed",
+      "beenz": 5,
+      "photo_url": "http://somephotourl.com/abed.jpg"
+    }
+  EOS
   def show
     u = params[:username].downcase == 'me' ? current_user : User.find_by_username(params[:username])
     raise NotFoundError.new("User not found") unless u
@@ -18,17 +26,39 @@ class UsersController < ApplicationController
   param :username, String, :required => true, :desc => "The desired username"
   param :password, String, :required => true
   error 403, "This username is already taken"
+  example <<-EOS
+    # The response will look like:
+    {
+      "username": "jeff",
+      "beenz": 1,
+      "token": "2309234fe1230d",
+      "photo_url": null
+    }
+  EOS
   def create
     raise UnauthorizedError.new("This username is already taken") if params[:username].downcase == "me" || User.find_by_username(params[:username])
+    
     u = User.new(username: params[:username])
     u.password = params[:password]
     u.save!
+
+    @current_user = u
+
     render :json => u, root: false
   end
 
   api :PUT, '/users/:username', "Update a user's password or upload a photo"
   param :password, String, :desc => "New password for user"
   param_group :token
+  example <<-EOS
+    # The response will look like:
+    {
+      "username": "jeff",
+      "beenz": 1,
+      "token": "2309234fe1230d",
+      "photo_url": "http://somephotourl.com/jeff.jpg"
+    }
+  EOS
   def update
     current_user.password = params[:password] if params[:password]
     current_user.photo = params[:photo] if params[:photo]
@@ -42,6 +72,15 @@ class UsersController < ApplicationController
   param :password, String, :required => true
   error 404, "Username not found"
   error 404, "Incorrect password"
+  example <<-EOS
+    # The response will look like:
+    {
+      "username": "jeff",
+      "beenz": 1,
+      "token": "2309234fe1230d",
+      "photo_url": "http://somephotourl.com/jeff.jpg"
+    }
+  EOS
   def login
     u = User.find_by_username(params[:username])
     raise UnauthorizedError.new("Username not found") unless u
@@ -50,11 +89,16 @@ class UsersController < ApplicationController
     u.generate_token
     u.save!
 
+    @current_user = u
+
     render :json => u, root: false
   end
 
   api :GET, '/users/logout', "Logout as a user"
   param_group :token
+  example <<-EOS
+  This will return a blank response with status code 200
+  EOS
   def logout
     current_user.generate_token
     current_user.save!
@@ -62,9 +106,17 @@ class UsersController < ApplicationController
     head :ok
   end
 
-  api :POST, '/users/:username/give', "Give meowmeowbeenz to a user"
-  param :beenz, 'Beenz', :required => true, :desc => "Number of meowmeowbeenz to give user, must be between 1-5"
+  api :POST, '/users/:username/give', "Give MeowMeowBeenz to a user"
+  param :beenz, 'Beenz', :required => true, :desc => "Number of MeowMeowBeenz to give user, must be between 1-5"
   param_group :token
+  example <<-EOS
+    # The response will look like (assuming you are giving beenz to Abed):
+    {
+      "username": "abed",
+      "beenz": 2.5,
+      "photo_url": "http://somephotourl.com/abed.jpg"
+    }
+  EOS
   def give
     u = User.find_by_username(params[:username])
     current_user.give_beenz_to_user(params[:beenz].to_i, u)
@@ -75,6 +127,21 @@ class UsersController < ApplicationController
   api :GET, '/users/search', "Search for users"
   param :query, String, :required => true
   param_group :token
+  example <<-EOS
+    # The response will look like:
+    [
+      {
+        "username": "abed",
+        "beenz": 5,
+        "photo_url": "http://somephotourl.com/abed.jpg"
+      },
+      {
+        "username": "annie",
+        "beenz": 4,
+        "photo_url": null
+      }
+    ]
+  EOS
   def search
     u = User.where("LOWER(username) LIKE ?", "%#{params[:query].downcase}%").where.not(id: current_user.id)
     render json: u, root: false
